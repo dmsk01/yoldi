954953f3548b66190b99f781724eba37c4e23d07
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2022-04-09 09:37:17 +0300

    header close on link click

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,4 +3,10 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+const menuLinks = document.querySelectorAll(".menu-tabs__link");
 
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.documentElement.classList.remove("menu-open");
+  });
+});
